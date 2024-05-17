a358e166715ff4a3d1f89aafe9cb5e8b37282860
DenMaz7 <91177753+DenMaz7@users.noreply.github.com>
2024-05-17 22:31:37 +0300

    add js folder

---
+++ js/AjaxifyJS.js
@@ -0,0 +1,40 @@
+(function (global) {
+    
+    const ajaxifyJS = {}
+
+    function getRequestObject() {
+        if(global.XMLHttpRequest) {
+            return (new XMLHttpRequest());
+        }
+        else {
+            global.alert("Ajax is not supported!");
+            return (null);
+        }
+    }
+
+    ajaxifyJS.sendGetRequest = function (requestUrl, responseHandler, isJsonResponse) {
+        let request = getRequestObject();
+        request.onreadystatechange = function() {
+            handleResponse(request, responseHandler, isJsonResponse);
+        };
+        request.open("GET", requestUrl, true);
+        request.send(null);
+    }
+
+    function handleResponse(request, responseHandler, isJsonResponse) {
+        if((request.readyState === 4) && (request.status === 200)) {
+            if(isJsonResponse === undefined) {
+                isJsonResponse = true;
+            }
+            if(isJsonResponse) {
+                responseHandler(JSON.parse(request.responseText));
+            }
+            else {
+                responseHandler(request.responseText);
+            }
+        }
+    }
+
+    global.$ajaxifyJS = ajaxifyJS;
+
+})(window)
