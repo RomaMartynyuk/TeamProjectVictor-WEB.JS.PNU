b1dcb98f266f1c1de567b2e7a6bfa67ac3ce5309
DenMaz7 <91177753+DenMaz7@users.noreply.github.com>
2024-05-21 21:56:42 +0200

    update

--- js/game.js
+++ js/game.js
@@ -3,6 +3,9 @@
     const vJS = {};
     
     vJS.StartGame = function () {
+        document.querySelector("body").innerHTML = '<div class="game-container"><p id="score">Score: 0</p><img src="images/game/spider-man.png" alt="Гравець" id="player"><div class="win-message" id="winMessage"><p>Ви перемогли!</p><button onclick="restartGame()">Переграти</button><button onclick="goToMain()">Наголовну</button></div></div>';
+
+
         const player = document.getElementById('player');
         let direction = 'right';
         const speed = 20;
